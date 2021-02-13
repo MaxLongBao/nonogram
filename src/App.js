d3663a3e0d11f43404a5f8e4c6659b73978110ad
Massimiliano Bisceglie <maxbisceglie89@gmail.com>
2021-02-13 19:15:27 +0000

    generator random grid value

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+
+  const generateMatrix = (length) => {
+    const fullOrEmpty = ['x', 'o'];
+    const grid = [];
+
+    for (let i = 0; i < length; i++) {
+      const rows = [];
+
+      for (let j = 0; j < length; j++) {
+        const randomElement = fullOrEmpty[Math.floor(Math.random() * fullOrEmpty.length)];
+        rows.push(randomElement);
+      }
+      grid.push(rows);
+    }
+    return grid;
+  }
+
+  const [matrix, setMatrix] = useState(generateMatrix(3));
+
+  console.log(matrix, 'matrix')
+
   return (
     <div className="App">
-      hello
+
     </div>
   );
 }
